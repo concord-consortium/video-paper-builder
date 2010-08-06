fd6af807bdf25d380aa4e6a8c0551bfb70b7c240
Jon Herman <jonherman@gmail.com>
2010-08-06 13:03:33 -0400

    updated section content column to text

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100804155640) do
+ActiveRecord::Schema.define(:version => 20100805172330) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                              :default => "", :null => false
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(:version => 20100804155640) do
 
   create_table "sections", :force => true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content"
     t.integer  "video_paper_id"
     t.datetime "created_at"
     t.datetime "updated_at"
