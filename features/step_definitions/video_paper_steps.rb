70b40c279fee2934c0068135c19c40706c91e773
patricksrobertson <patricksrobertson@gmail.com>
2010-08-09 22:46:44 -0400

    got tests to pass

--- features/step_definitions/video_paper_steps.rb
+++ features/step_definitions/video_paper_steps.rb
@@ -1,6 +1,6 @@
 Then /^I create a new video paper named "([^\"]*)"$/ do |title|
   fill_in "Title", :with => title
-  click_button "Create"
+  click_button "Enter in notes"
 end
 
 When /^I edit the video paper title named "([^\"]*)"$/ do |title|
