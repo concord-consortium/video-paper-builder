85c69366b3a21ff927299bd9406fe57572cfca15
Scott Cytacki <scytacki@concord.org>
2012-03-08 12:05:06 -0500

    add a gateway to make it easy to deploy from home

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,10 @@
 set :application, "vpb"
 set :branch,  source_branch('development')
 set :deploy_to, "/var/www/#{application}/production"
+set :gateway, "otto.concord.org"
 set :rails_env,:production
 set :user, 'deploy'
+
 # set :password
 # Add public keys to the deploy users authorized list.
 server 'kaltura-webapp.concord.org', :app, :web, :db, :primary=>true
