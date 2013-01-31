6af3c23b4bcf52824d258bcf34bf2524ad75e486
Scott Cytacki <scytacki@concord.org>
2013-01-31 00:19:59 -0500

    test env needs host name for fake emails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,7 @@ VPB::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # set a host name for emails
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
