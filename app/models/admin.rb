74585c898737fd6bd684022271649f05f7623ef9
Scott Cytacki <scytacki@concord.org>
2013-01-31 00:27:10 -0500

    remove now unnecessary hack

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -32,28 +32,7 @@ class Admin < ActiveRecord::Base
     "#{self.first_name.downcase.titlecase} #{self.last_name.downcase.titlecase}"
   end  
   
-  ##################################
-  # Class Methods
-  #################################
-  class << self
-    ##
-    # This is a bit of an ugly hack.  The deal is this.  The devise invitable method
-    # creates a new user account with just the email.  We need it to create with a first/last name
-    # too.  So here you are.
-    def send_invitation(attributes={})
-      invitable = self.find_or_initialize_by_email(attributes[:email])
-
-      if invitable.new_record?
-        invitable.first_name = attributes[:first_name]
-        invitable.last_name = attributes[:last_name]
-        invitable.errors.add(:email, :blank) if invitable.email.blank?
-        invitable.errors.add(:email, :invalid) unless invitable.email.match /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
-      else
-        invitable.errors.add(:email, :taken) unless invitable.invited?
-      end
-
-      invitable.resend_invitation! if invitable.errors.empty?
-      invitable
-    end
-  end  
+  def self.invite_key_fields
+    [:first_name, :last_name, :email]
+  end
 end
