61998b8f6991af470a3f70fe116761d1d268e288
Scott Cytacki <scytacki@concord.org>
2013-02-03 00:52:32 -0500

    update migrations to work from scratch with new devise
    the migration had devise helpers in them which are not supported anymore
    and even if the helpers were supported they would create different fields now

--- db/migrate/20100728173043_devise_create_users.rb
+++ db/migrate/20100728173043_devise_create_users.rb
@@ -1,15 +1,24 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
-      t.confirmable
-      t.recoverable
-      t.rememberable
-      t.trackable
-      t.invitable
-      # t.lockable
-
-      t.timestamps
+      t.string   "email",                                :default => "", :null => false
+      t.string   "encrypted_password",   :limit => 128,  :default => "", :null => false
+      t.string   "password_salt",                        :default => "", :null => false
+      t.string   "confirmation_token"
+      t.datetime "confirmed_at"
+      t.datetime "confirmation_sent_at"
+      t.string   "reset_password_token"
+      t.string   "remember_token"
+      t.datetime "remember_created_at"
+      t.integer  "sign_in_count",                        :default => 0
+      t.datetime "current_sign_in_at"
+      t.datetime "last_sign_in_at"
+      t.string   "current_sign_in_ip"
+      t.string   "last_sign_in_ip"
+      t.string   "invitation_token",       :limit => 30
+      t.datetime "invitation_sent_at"
+      t.datetime "created_at"
+      t.datetime "updated_at"
     end
 
     add_index :users, :email,                :unique => true
