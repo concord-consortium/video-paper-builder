8bda33366acab78ea4c4c7810d52e49cdd7d3e11
Scott Cytacki <scytacki@concord.org>
2013-02-06 00:25:51 -0500

    change paperclip path files to match legacy location
    thumbnails have been changed on the server to match the default location

--- app/models/wysihat_file.rb
+++ app/models/wysihat_file.rb
@@ -1,5 +1,5 @@
 class WysihatFile < ActiveRecord::Base
-  has_attached_file :file
+  has_attached_file :file, :path => ':rails_root/public/system/:attachment/:id/:style/:filename'
   
   belongs_to :user
 end
