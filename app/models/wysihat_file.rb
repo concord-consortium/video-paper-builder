cf9f64c2661d376cc381cf94191f7ca7b47e1483
Scott Cytacki <scytacki@concord.org>
2013-02-06 00:47:49 -0500

    need to set url too after changing path

--- app/models/wysihat_file.rb
+++ app/models/wysihat_file.rb
@@ -1,5 +1,7 @@
 class WysihatFile < ActiveRecord::Base
-  has_attached_file :file, :path => ':rails_root/public/system/:attachment/:id/:style/:filename'
+  has_attached_file :file,
+    :path => ':rails_root/public/system/:attachment/:id/:style/:filename',
+    :url => '/system/:attachment/:id/:style/:filename'
   
   belongs_to :user
 end
