afc8befb0fe50353ede51f8d02c0f8f38a95a60a
Doug Martin <doug@dougmart.in>
2016-02-01 11:04:42 -0500

    Changed video factory to use upload_uri instead of Kaltura entry id

--- spec/factories/video.rb
+++ spec/factories/video.rb
@@ -1,12 +1,11 @@
 FactoryGirl.define do
   factory :video do
-    entry_id 'fake_entry_id'
+    upload_uri 'uploads/test.mov'
     association :video_paper
     private false
   end
 
   factory :real_video, :parent => :video do
-    entry_id 1 # TODO: update for AWS
     association :video_paper
     description "this is an awesome description"
     private false
