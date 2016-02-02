9e005c727a278e82ba225c7759cbb0135c81ead7
Doug Martin <doug@dougmart.in>
2016-02-02 13:05:23 -0500

    Changed help pages to use video.js and default to loading from models-resources.concord.org

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,15 @@
 module HomeHelper
+  def embed_help_video(video)
+    return <<-html
+    <div id="help_video_player_container">
+      <video id="help_video_player" class="video-js vjs-default-skin" controls preload="auto" width="auto" height="auto">
+        <source src="#{ENV['HELP_VIDEO_FOLDER_URL'] || '//models-resources.concord.org/vpb/help'}/#{video}.mp4" type="video/mp4" />',
+      </video>
+    </div>
+    <script>
+      videojs("help_video_player_container");
+    </script>
+    <noscript>Please enable Javascript.</noscript>
+    html
+  end
 end
