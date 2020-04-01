3fd64a6ec856fef1bc26dbfa649025052e93961c
Doug Martin <dmartin@concord.org>
2020-04-01 14:24:40 -0400

    Update to ruby 2.5.1

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,12 @@
+//= link application.js
+//= link application.css
+
+//= link bottom_arrow_active.gif
+//= link bottom_arrow_normal.gif
+//= link top_arrow_normal.gif
+//= link top_arrow_active.gif
+//= link bottom_arrow_active.gif
+//= link blank_fallback.png
+//= link big_pencil_icon.png
+//= link next_arrow.gif
+//= link pre_arrow.gif
